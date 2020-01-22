cb9e615e25b85fb418980bf0986afed51a3dce89
TripleZer0 <57972505+TripleZer000@users.noreply.github.com>
2020-01-22 11:09:20 -0600

    stupid env vars

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.DISCORD_BOT_SECRET;
 const url = require('./url.js');
+require('dotenv').config();
 var irc = require('irc');
-var bot = new irc.Client('irc_server', 'irc_nick', {
-    password: ('irc_pass'),
-    channels: ['irc_channel'],
+var bot = new irc.Client(process.env.irc_server, process.env.nick, {
+    password: (process.env.pass),
+    channels: ['#' + process.env.channel],
 });
 
 bot.addListener('error', function(message) {
@@ -19,17 +20,17 @@ bot.addListener('message#blah', function(from, message) {
 /// YOOOOOOOOOOOOOOOOOOOO edit discord channel ids
 
 bot.addListener('action', function (from, to, text, message) {
-    client.channels.get('discord_id').send('***'+ from + ' ' + text + '***')
+    client.channels.get(process.env.discord_id).send('***'+ from + ' ' + text + '***')
 });
 bot.addListener('join', function(channel, nick, message) {
     console.log(nick)
-    client.channels.get('discord_id').send('**```' + nick + ' has joined IRC #lobby      [irc.evilcorp.ga]```**');
+    client.channels.get(process.env.discord_id).send('**```' + nick + ' has joined IRC ' + '#' + process.env.channel + '      [' + process.env.irc_server + ']```**');
 });
 bot.addListener('quit', function(nick) {
-    client.channels.get('discord_id').send('**```' + nick + ' has left IRC #lobby      [irc.evilcorp.ga]```**');
+    client.channels.get(process.env.discord_id).send('**```' + nick + ' has left IRC ' + '#' +  process.env.channel + '      [' +  process.env.irc_server + ']```**');
 });
 bot.addListener('kick', function(channel, who, by, reason) {
-    client.channels.get('discord_id').send('**```' + who + ' was kicked by ' + by + '      [irc.evilcorp.ga]```**');
+    client.channels.get(process.env.discord_id).send('**```' + who + ' was kicked by ' + by + '      [' + process.env.irc_server + ']```**');
 });
 
   // discord
@@ -40,25 +41,22 @@ client.on('ready', () => {
 
 client.on('message', message =>
 {
- if (message.author.username === 'irc_nick' || message.channel.name != 'irc_channel') {
+ if (message.author.username === process.env.nick || message.channel.name != process.env.channel) {
  }
  else {
-         bot.say('irc_#channel', '[' + message.author.username + ']: ' + message.content)
+         bot.say('#' + process.env.channel, '[' + message.author.username + ']: ' + message.content)
  }
 });
 bot.addListener('message', function(from, to, message) 
 {
     console.log('%s => %s: %s', from, to, message);
-    client.channels.get('discord_id').send('[' + from + ']: ' + message);
+    client.channels.get(process.env.discord_id).send('[' + from + ']: ' + message);
 });
 
- // !info DarkLair
-/// client.on('message', msg => {
-///  if (msg.content === '!info DarkLair') {
-///  exec(url)
-///  });
 
-/////
+//
+///
+////
 
 
 client.login(token);
